a6f761349ac6a3672577767daadebe24c98f5f33
Nguyễn Ngọc Thương <57335014+thuongnguyen01685@users.noreply.github.com>
2022-03-23 18:09:42 +0700

    delete dư styleSheet

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
+
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home/Home';
 
