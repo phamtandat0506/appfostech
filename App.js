53db62e13d8325c382e27245ba58f45246034e93
TanDat <phamtandat05062000@gmail.com>
2022-03-23 15:38:28 +0700

    home.js

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home/Home';
+
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+            headerStyle: {
+              backgroundColor: '#fff',
+              fontSize: '32',
+              fontWeight: 800,
+            },
+            headerTintColor: 'black',
+            
+          }}
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={Home}
+          options={{ 
+            title: 'Home',
+            headerStyle: {
+              backgroundColor: '#fff',
+            },
+            headerTintColor: '#000',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 32,
+            },
+          }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +50,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+// <Stack.Screen 
+//           name="Search" 
+//           component={Search}
+//         />
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,1 +1,10 @@
-a
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Search({ navigation }) {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  )
+}
